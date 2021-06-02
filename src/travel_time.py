a73f70496e94a735ff8bb96f09d74147466c683f
Edrian <edrian@us.ibm.com>
2021-06-02 11:48:18 -0400

    Making a long line smaller
    Signed-off-by: Edrian <edrian@us.ibm.com>

--- src/travel_time.py
+++ src/travel_time.py
@@ -166,7 +166,8 @@ def get_travel_time(params):
         else:
             travel_time_fit = False
             print(
-                "  Saved travel time table does not match the parameters given in the parameter file."
+                "  Saved travel time table does not match the parameters"
+                " given in the parameter file."
             )
 
     except:
